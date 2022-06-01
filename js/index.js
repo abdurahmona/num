fae472f36841574f0a77e2e95c8646adc010c688
abdurahmona <aburashidoff7@gmail.com>
2022-06-01 08:05:51 +0300

    num

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+
+const numbers = [2, 3, 8, 18]
+
+const numbersSum = function(arr){
+
+    let son = 0
+
+    for ( let num of numbers){
+
+        son += num
+    }
+
+    return son
+}
+
+console.log(numbersSum(numbers));
