a8b3767de35f2287d07abb7d16c85b4f5bd37658
abdurahmona <aburashidoff7@gmail.com>
2022-06-13 07:12:40 +0300

    aaa

--- js/index.js
+++ js/index.js
@@ -1,17 +1,44 @@
 
 
-const numbers = [2, 3, 8, 18]
+// const numbers = [2, 3, 8, 18]
 
-const numbersSum = function(arr){
+// const numbersSum = function(arr){
 
-    let son = 0
+//     let son = 0
 
-    for ( let num of numbers){
+//     for ( let num of numbers){
 
-        son += num
-    }
+//         son += num
+//     }
 
-    return son
+//     return son
+// }
+
+// console.log(numbersSum(numbers));
+
+
+// const abu = {
+// name: "abu",
+// friends: ["azim", "ibrohim"]
+// }
+
+// console.log(``);
+
+const elform = document.querySelector(".form")
+
+const elinput = document.querySelector(".input")
+
+elform.addEventListener('submit', function(evt){
+evt.preventDefault()
+
+const inputvalue = elinput.value
+
+const todo = {
+    name: inputvalue
 }
 
-console.log(numbersSum(numbers));
+todos.push(todo)
+
+console.log(todos);
+
+})
